d5d339e3af9dfd6339d0139970c8c8c2d116bdb0
Mateusz Skóra <mateusz.skora@microsoft.wsei.edu.pl>
2025-10-04 09:29:41 +0200

    Zadanie 1
    Zrobione zadanie 1 i troche 2 ( wiec modyfikacja 2)

---
+++ WebApp1/WebApp1/Controllers/BlogController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ILogger<BlogController> _logger;
+        private static readonly List<BlogViewModel> _articles = new()
+        {
+            new BlogViewModel
+            {
+                Title = "My First Blog Post",
+                Description = "This is the content of my first blog post.",
+                
+            },
+            new BlogViewModel
+            {
+                Title = "My Second Blog Post",
+                Description = "This is the content of my second blog post.",
+            },
+            new BlogViewModel
+            {
+                Title = "My Third Blog Post",
+                Description = "This is the content of my third blog post.",
+            }
+
+        };
+        public BlogController(ILogger<BlogController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View(new BlogViewModel()
+            {
+                Title = "My First Blog Post",
+                Description = "This is the content of my first blog post.",
+                
+
+            });
+        }
+    }
+}
---
+++ WebApp1/WebApp1/Models/BlogViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1.Models
+{
+    public class BlogViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
